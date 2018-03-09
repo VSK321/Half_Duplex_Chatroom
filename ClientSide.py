cdde396db74b27a878994fc6eac24f7158dd696c
VSK321 <37224049+VSK321@users.noreply.github.com>
2018-03-09 18:52:06 +0000

    Update ClientSide.py

--- ClientSide.py
+++ ClientSide.py
@@ -1,11 +1,11 @@
 import socket
 
-LocalIPAddress = "127.0.0.1" #Enter your local ip address here
-port = 444 #Enter a port number to bind the socket to. This can be an integer from 1 to 65535
+LocalIPAddress = "" #Enter your local ip address here
+port = 0 #Enter a port number to bind the socket to. This can be an integer from 1 to 65535
 data = ""
 request = ""
-ServerIPAddress = "127.0.0.1" #Enter the local ip address of the server
-ServerPort = 44 #Enter the port on the server side
+ServerIPAddress = "" #Enter the local ip address of the server
+ServerPort =  #Enter the port on the server side
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
 sock.bind((LocalIPAddress, port))
